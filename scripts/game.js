4c0eb7281d6db5a1628684f164d346187e4a61ec
sonetto104 <peter_regan88@yahoo.com>
2023-05-08 20:22:04 +0000

    Restart jest practice.

--- scripts/game.js
+++ scripts/game.js
@@ -1,33 +1,6 @@
 let game = {
-    currentGame: [],
-    playerMoves: [],
     score: 0,
-    choices: ["button1", "button2", "button3", "button4"]
+    currentGame: [],
 };
 
-function newGame() {
-    game.score = 0;
-    game.currentGame = [];
-    game.playerMoves = [];
-    showScore();
-    addTurn();
-}
-
-function addTurn() {
-    game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    /*showTurns()*/
-}
-
-function showScore() {
-    document.getElementById("score").innerText = game.score
-};
-
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light");
-    }, 400);
-};
-
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+module.exports = { game };
